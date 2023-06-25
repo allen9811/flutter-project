ee679c138d5b6e4880c2ba141aa43a6c72932708
Efrain Colan <ef.colan@gmail.com>
2023-06-25 13:10:14 +0200

    jpa validate fix

--- backend/src/main/java/com/pradera/backend/model/User.java
+++ backend/src/main/java/com/pradera/backend/model/User.java
@@ -2,13 +2,13 @@ package com.pradera.backend.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import org.springframework.data.domain.Auditable;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "USER")
+@Table(name = "\"USER\"")
 @Data
 public class User {
 
@@ -26,7 +26,7 @@ public class User {
     private LocalDateTime birthdate;
     @Column(name="EMAIL")
     private String email;
-    @Column(name="PHONE NUMBER")
+    @Column(name="PHONE_NUMBER")
     private String phoneNumber;
     @Column(name="COUNTRY")
     private String country;
