ef20f04c1ebbd0397b4de0de930bf1bd3e14a933
Efrain Colan <ef.colan@gmail.com>
2023-06-29 08:04:50 +0200

    User creation endpoint + repository

--- backend/src/main/java/com/pradera/backend/controller/UserController.java
+++ backend/src/main/java/com/pradera/backend/controller/UserController.java
@@ -2,10 +2,15 @@ package com.pradera.backend.controller;
 
 import com.pradera.backend.model.UserDAO;
 import com.pradera.backend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -16,12 +21,18 @@ public class UserController {
 
     @PostMapping("/create")
     public String createUser(@RequestBody UserDAO user) {
+        log.info("User creation started with incoming body: {}", user);
         return userService.createUser(user);
     }
 
     @GetMapping("/info/{userId}")
-    public UserDAO getUserInformation(@PathVariable String userId) {
+    public UserDAO getUserInformation(@PathVariable("userId") String userId) {
+        log.info("User information extraction started with param: {}", userId);
         // TODO: Add security validation
+        if (StringUtils.isBlank(userId)) {
+            log.error("User id can not be blank");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User id can not be blank");
+        }
         return userService.extractUserInformation(userId);
     }
 
